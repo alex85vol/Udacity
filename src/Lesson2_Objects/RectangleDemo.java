769c46a215bc03b5cd4a31f4adba80ba9fa8a921
unknown <ovo@sheridan.softservecom.com>
2016-01-22 18:12:40 +0200

    mutators and acessors

--- src/Lesson2_Objects/RectangleDemo.java
+++ src/Lesson2_Objects/RectangleDemo.java
@@ -11,6 +11,61 @@ public class RectangleDemo
 {
     public static void main(String[] args)
     {
+        /*Rectangle box = new Rectangle(60, 90, 20, 30);
+        Rectangle box2 = new Rectangle(80, 120, 20, 30);
+
+        box.draw();
+        box2.draw();*//*
         // TODO: Construct and draw the rectangles
+        Rectangle box = new Rectangle(5, 10, 60, 90);
+        *//*System.out.println(box.getX());
+        System.out.println(box.getWidth());
+        box.translate(25, 40);
+        System.out.println(box.getX());
+        System.out.println(box.getWidth());*//*
+        Rectangle box2 = box;
+        box.setColor(Color.RED);
+        box2.setColor(Color.BLUE);
+        box.fill();*/
+
+        Rectangle box = new Rectangle(45, 90, 60, 90);
+        System.out.println("Before");
+        System.out.println("X is: " + box.getX());
+        System.out.println("Y is: " + box.getY());
+        System.out.println("Width is: " + box.getWidth());
+        System.out.println("Height is: " + box.getHeight());
+        System.out.println();
+        box.grow(20, 20);
+
+        // You need to print out the following values of box and its expected values:
+        //
+        System.out.println("After");
+        System.out.println("X is: " + box.getX());
+        System.out.println("Expected X: " + 25);
+        System.out.println("Y is: " + box.getY());
+        System.out.println("Expected Y: " + 70);
+        System.out.println("Width is: " + box.getWidth());
+        System.out.println("Expected width: " + 100);
+        System.out.println("Height is: " + box.getHeight());
+        System.out.println("Expected height: " + 130);
+        System.out.println();
+        // X, by using box.getX();
+        // Y, by using box.getY();
+        // width, by using box.getWidth();
+        // height, by using box.getHeight();
+        //
+        // To print out a value and its expected value, use System.out.println(...);
+        // For example:
+        // System.out.println(box.getY());
+        // System.out.println("Expected: " + 50);
+        //
+        // Print out the actual and test values below this line:
+
+
+
+
+
+
+        }
     }
-}
+
